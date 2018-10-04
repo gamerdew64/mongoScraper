7cda637c1b7ffbca76bb7b80fec24a7bc05dd6c9
Joseph Perkins <gamerdew64@gmail.com>
2018-10-03 21:50:09 -0500

    added sorting when pulling articles

--- server.js
+++ server.js
@@ -79,7 +79,9 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({}).limit(50)
+  db.Article.find({}, null, {sort:{"_id":-1}}).limit(50)
+
+
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
